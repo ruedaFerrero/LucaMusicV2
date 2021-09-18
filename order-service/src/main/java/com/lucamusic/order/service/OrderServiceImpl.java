3b8f19431f8518606ced44b11ad7279fa857bbee
manu <manueli_rubi_00@hotmail.com>
2021-09-18 11:52:27 +0200

    Order config

--- order-service/src/main/java/com/lucamusic/order/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/lucamusic/order/service/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package com.lucamusic.order.service;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.lucamusic.order.entity.Order;
 import com.lucamusic.order.repository.OrderRepository;
 
+@Service
 public class OrderServiceImpl {
 
 
@@ -14,17 +16,18 @@ public class OrderServiceImpl {
 	OrderRepository repo;
 
 	public Order createOrder(Order order) {
-		Order orders=repo.getById(order.getId());
-		if(order!=null) {
-			return orders;
+		Order orders;
+		if(order.getId()!=null) {
+			orders=repo.getById(order.getId());
+			if(orders!=null) {
+				return orders;
+			}
 		}
+		
 		order.setStatus("CREATED");
 		return repo.save(order);
 	}
 
 	
-	public Order validateOrder(Order order) {
-		return order;
-		
-	}
+	
 }
