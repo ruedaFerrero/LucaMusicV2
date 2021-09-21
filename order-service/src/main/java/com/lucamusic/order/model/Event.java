c1eb9615003d35bcf823083c891981e6adced295
Julio <julio@ruedaferrero.com>
2021-09-21 20:26:56 +0200

    Added javadoc

--- order-service/src/main/java/com/lucamusic/order/model/Event.java
+++ order-service/src/main/java/com/lucamusic/order/model/Event.java
@@ -1,21 +1,9 @@
 package com.lucamusic.order.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-
-import javax.validation.Valid;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 
-import java.time.LocalDate;
-import java.util.Map;
 /**
  * Event
  * Clase entidad del Objeto Event
