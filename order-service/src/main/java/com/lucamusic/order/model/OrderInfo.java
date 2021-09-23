b6bfc299f4f7aae1034aaa6ddeedd37b50d70072
José Antonio <delosangelesrodriguezjose@gmail.com>
2021-09-23 11:24:51 +0200

    Limpieza 2

--- order-service/src/main/java/com/lucamusic/order/model/OrderInfo.java
+++ order-service/src/main/java/com/lucamusic/order/model/OrderInfo.java
@@ -11,8 +11,6 @@ import lombok.Data;
  */
 @Data
 public class OrderInfo {
-//    private User user;
-//    private Event event;
     private PaymentInfo paymentInfo;
     private int numTickets;
 }
